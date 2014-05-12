7bf7f865af3f3e10318a23015c36e29fe3ee2b0e
Dave Crossland <dave@lab6.com>
2014-05-12 17:41:19 -0400

    Renaming files

--- Source/Autohint.bat
+++ Source/Autohint.bat
@@ -7,55 +7,55 @@
 :: We are using the older ttfautohint, we will switch to the newer version, once support for Devanagari is added.
 
 :: Make sure shipped files have names given below
-ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "01 Ek Mukta Extralight_Shipped.ttf" "01 Ek Mukta ExtraLight hinted.ttf"
-ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "02 Ek Mukta Light_Shipped.ttf" "02 Ek Mukta Light hinted.ttf"
-ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "03 Ek Mukta Regular_Shipped.ttf" "03 Ek Mukta Regular hinted.ttf"
-ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "04 Ek Mukta Medium_Shipped.ttf" "04 Ek Mukta Medium hinted.ttf"
-ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "05 Ek Mukta Semibold_Shipped.ttf" "05 Ek Mukta Semibold hinted.ttf"
-ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "06 Ek Mukta Bold_Shipped.ttf" "06 Ek Mukta Bold hinted.ttf"
+ttfautohint --composites --fallback-script=latn --verbose --windows-compatibility --increase-x-height=0 --hinting-limit=0 --no-info --strong-stem-width=gGD "EkMukta-Extralight_Shipped.ttf" "EkMukta-ExtraLight hinted.ttf"
+ttfautohint --composites --fallback-script=latn --verbose --windows-compatibility --increase-x-height=0 --hinting-limit=0 --no-info --strong-stem-width=gGD "EkMukta-Light_Shipped.ttf" "EkMukta-Light hinted.ttf"
+ttfautohint --composites --fallback-script=latn --verbose --windows-compatibility --increase-x-height=0 --hinting-limit=0 --no-info --strong-stem-width=gGD "EkMukta-Regular_Shipped.ttf" "EkMukta-Regular hinted.ttf"
+ttfautohint --composites --fallback-script=latn --verbose --windows-compatibility --increase-x-height=0 --hinting-limit=0 --no-info --strong-stem-width=gGD "EkMukta-Medium_Shipped.ttf" "EkMukta-Medium hinted.ttf"
+ttfautohint --composites --fallback-script=latn --verbose --windows-compatibility --increase-x-height=0 --hinting-limit=0 --no-info --strong-stem-width=gGD "EkMukta-Semibold_Shipped.ttf" "EkMukta-Semibold hinted.ttf"
+ttfautohint --composites --fallback-script=latn --verbose --windows-compatibility --increase-x-height=0 --hinting-limit=0 --no-info --strong-stem-width=gGD "EkMukta-Bold_Shipped.ttf" "EkMukta-Bold hinted.ttf"
 
 :: Currently ttfautohint results for Extrabold are poor, so we dont hint the Extrabold
-REM ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "07 Ek Mukta ExtraBold_Shipped.ttf" "07 Ek Mukta ExtraBold hinted.ttf"
+REM ttfautohint -c -f latn -v -W -x 0 -G 0 -n -w gGD "EkMukta-Extrabold_Shipped.ttf" "EkMukta-Extrabold hinted.ttf"
 
 
 :: Copy Hinted files to Binaries folder
 
-copy "01 Ek Mukta Extralight hinted.ttf" ..\TTF\"01 Ek Mukta Extralight.ttf" 
-copy "02 Ek Mukta Light hinted.ttf" ..\TTF\"02 Ek Mukta Light.ttf" 
-copy "03 Ek Mukta Regular hinted.ttf" ..\TTF\"03 Ek Mukta Regular.ttf" 
-copy "04 Ek Mukta Medium hinted.ttf" ..\TTF\"04 Ek Mukta Medium.ttf" 
-copy "05 Ek Mukta Semibold hinted.ttf" ..\TTF\"05 Ek Mukta Semibold.ttf" 
-copy "06 Ek Mukta Bold hinted.ttf" ..\TTF\"06 Ek Mukta Bold.ttf" 
+copy "EkMukta-Extralight hinted.ttf" ..\TTF\"EkMukta-Extralight.ttf" 
+copy "EkMukta-Light hinted.ttf" ..\TTF\"EkMukta-Light.ttf" 
+copy "EkMukta-Regular hinted.ttf" ..\TTF\"EkMukta-Regular.ttf" 
+copy "EkMukta-Medium hinted.ttf" ..\TTF\"EkMukta-Medium.ttf" 
+copy "EkMukta-Semibold hinted.ttf" ..\TTF\"EkMukta-Semibold.ttf" 
+copy "EkMukta-Bold hinted.ttf" ..\TTF\"EkMukta-Bold.ttf" 
 :: Copy ExtraBold as it is
-copy "07 Ek Mukta ExtraBold_Shipped.ttf" ..\TTF\"07 Ek Mukta ExtraBold.ttf" 
+copy "EkMukta-Extrabold_Shipped.ttf" ..\TTF\"EkMukta-Extrabold.ttf" 
 
 :: Clean Up
 
 :: First the Shipped files
 
-del "01 Ek Mukta Extralight_Shipped.ttf"
-del "02 Ek Mukta Light_Shipped.ttf"
-del "03 Ek Mukta Regular_Shipped.ttf"
-del "04 Ek Mukta Medium_Shipped.ttf"
-del "05 Ek Mukta Semibold_Shipped.ttf"
-del "06 Ek Mukta Bold_Shipped.ttf"
-del "07 Ek Mukta ExtraBold_Shipped.ttf"
+del "EkMukta-Extralight_Shipped.ttf"
+del "EkMukta-Light_Shipped.ttf"
+del "EkMukta-Regular_Shipped.ttf"
+del "EkMukta-Medium_Shipped.ttf"
+del "EkMukta-Semibold_Shipped.ttf"
+del "EkMukta-Bold_Shipped.ttf"
+del "EkMukta-Extrabold_Shipped.ttf"
 
 :: The Base files
 
-del "01 Ek Mukta Extralight.ttf" 
-del "02 Ek Mukta Light.ttf" 
-del "03 Ek Mukta Regular.ttf" 
-del "04 Ek Mukta Medium.ttf" 
-del "05 Ek Mukta Semibold.ttf" 
-del "06 Ek Mukta Bold.ttf" 
-del "07 Ek Mukta ExtraBold.ttf" 
+del "EkMukta-Extralight.ttf" 
+del "EkMukta-Light.ttf" 
+del "EkMukta-Regular.ttf" 
+del "EkMukta-Medium.ttf" 
+del "EkMukta-Semibold.ttf" 
+del "EkMukta-Bold.ttf" 
+del "EkMukta-Extrabold.ttf" 
 
 :: Finally the Temporary Hinted files
 
-del "01 Ek Mukta Extralight hinted.ttf" 
-del "02 Ek Mukta Light hinted.ttf" 
-del "03 Ek Mukta Regular hinted.ttf" 
-del "04 Ek Mukta Medium hinted.ttf" 
-del "05 Ek Mukta Semibold hinted.ttf" 
-del "06 Ek Mukta Bold hinted.ttf" 
+del "EkMukta-Extralight hinted.ttf" 
+del "EkMukta-Light hinted.ttf" 
+del "EkMukta-Regular hinted.ttf" 
+del "EkMukta-Medium hinted.ttf" 
+del "EkMukta-Semibold hinted.ttf" 
+del "EkMukta-Bold hinted.ttf" 
